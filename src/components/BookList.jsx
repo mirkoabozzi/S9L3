da68f53b16b981bc5e1d9256d0ba34abd1eb2fc7
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-06-26 15:18:23 +0200

    BookList to class + raccolta dati utente

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,16 +1,26 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Form, InputGroup, Row } from "react-bootstrap";
 import SingleBook from "./SingleBook";
+import { Component } from "react";
 
-const BookList = (props) => {
-  return (
-    <Container>
-      <Row className="">
-        {props.books.map((book) => (
-          <SingleBook key={book.asin} img={book.img} title={book.title} price={book.price} />
-        ))}
-        ;
-      </Row>
-    </Container>
-  );
-};
+class BookList extends Component {
+  state = {
+    userBook: "",
+  };
+
+  render() {
+    return (
+      <Container>
+        <InputGroup className="mb-3">
+          <Form.Control type="text" placeholder="Cerca un libro" aria-describedby="basic-addon2" value={this.state.userBook} onChange={(e) => this.setState({ userBook: e.target.value })} />
+        </InputGroup>
+        <Row className="">
+          {this.props.books.map((book) => (
+            <SingleBook key={book.asin} img={book.img} title={book.title} price={book.price} />
+          ))}
+          ;
+        </Row>
+      </Container>
+    );
+  }
+}
 export default BookList;
