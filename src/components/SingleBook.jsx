c69fc5311decef607cfc7b5b6ae7f44200a5c39d
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-06-26 17:00:30 +0200

    consegna

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -9,7 +9,7 @@ class SingleBook extends Component {
   render() {
     return (
       <Col key={this.props.asin} sm="12" md="6" lg="4" xxl="3" className="my-3">
-        <Card classNaneme="my-3 shadow" onClick={() => this.setState({ selected: "secondary" })}>
+        <Card className="my-3 shadow" onClick={() => this.setState({ selected: "secondary" })}>
           <Card.Img variant="top" style={{ aspectRatio: 3 / 4 }} src={this.props.img} />
           <Card.Body>
             <Card.Title className="text-truncate">{this.props.title}</Card.Title>
