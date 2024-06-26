03389a0679003726ff5981485892411a302ff499
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-06-26 17:15:41 +0200

    toggle colore bottone al click della card

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -3,18 +3,18 @@ import { Button, Card, Col } from "react-bootstrap";
 
 class SingleBook extends Component {
   state = {
-    selected: "primary",
+    selected: false,
   };
 
   render() {
     return (
       <Col key={this.props.asin} sm="12" md="6" lg="4" xxl="3" className="my-3">
-        <Card className="my-3 shadow" onClick={() => this.setState({ selected: "secondary" })}>
+        <Card className="my-3 shadow" onClick={() => this.setState({ selected: !this.state.selected })}>
           <Card.Img variant="top" style={{ aspectRatio: 3 / 4 }} src={this.props.img} />
           <Card.Body>
             <Card.Title className="text-truncate">{this.props.title}</Card.Title>
             <Card.Text>{this.props.price} €</Card.Text>
-            <Button variant={this.state.selected}>Compra ora</Button>
+            <Button variant={this.state.selected ? "secondary" : "primary"}>Compra ora</Button>
           </Card.Body>
         </Card>
       </Col>
